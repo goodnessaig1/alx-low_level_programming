b53de47197a08ad8fa0b7f979cafcaca69f67b24
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-12 23:32:53 +0100

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+
+putchar ('\n');
+return (0);
+}
