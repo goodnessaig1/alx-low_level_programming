e28dd44eacfc0bd9f346424cbb362a277361aa02
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-12 13:24:51 +0100

    push

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,13 @@
 * Return: Always 0 (Success/correct)
 */
 int main(void)
+
 {
-    int i;
-    for (i = 0 ; i < 10; i++)
-    {
-        printf ("%i", i);
-    }
-    printf ("\n");
-    return (0);
+int i;
+for (i = 0 ; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
 }
