2cd5a0f97141928ecc79614bbd29dc0a12d7db14
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-12 13:30:57 +0100

    olk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+
+{
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+return(0);
+}
