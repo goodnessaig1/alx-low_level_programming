7fb772996c0f88fcd511a080108745b16e35e3e0
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-12 13:04:00 +0100

    column

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 {
 char lower_case;
 char upper_case;
-for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++);
 {
 putchar (lower_case); 
 }
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++);
 {
 putchar (upper_case);
 }
