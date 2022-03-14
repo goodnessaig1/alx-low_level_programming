2186392425e05a43fa67d7c91b98453c4fffed7a
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-14 21:31:28 +0100

    space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n == 0)
 {
-printf("%i is zero\n", n);
+printf ("%i is zero\n", n);
 }
 else if (n < 0)
 {
-printf("%i is negative\n", n);
+printf ("%i is negative\n", n);
 }
 else
 {
-printf("%i is positive\n", n);
+printf ("%i is positive\n", n);
 }
 return (0);
 }
