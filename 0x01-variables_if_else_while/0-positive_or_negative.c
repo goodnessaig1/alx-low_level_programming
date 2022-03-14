5e3cf5a2eea9cbd500fa890de0e750f7554939dc
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-14 21:27:52 +0100

    push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n == 0){
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
@@ -22,8 +22,9 @@ else if (n < 0)
 printf("%i is negative\n", n);
 }
 else
-printf("%i is positive\n", n);
 {
+printf("%i is positive\n", n);
+}
 return (0);
 
 }
