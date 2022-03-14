8746206d602d8cd4cad5adb64d9685db6e3d0faf
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-14 21:55:29 +0100

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,23 +2,45 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
  * Return: 0
  */
 int main(void)
 {
-	int c = 0;
+	int c;
+	int d = 0;
 
-	while (c < 10)
+	while (d < 10)
 	{
-		putchar(48 + c);
-		if (c != 9)
+		c = 0;
+		while (c < 10)
 		{
-			putchar(',');
-			putchar(' ');
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
 		}
-		c++;
+		d++;
 	}
 	putchar('\n');
 	return (0);
