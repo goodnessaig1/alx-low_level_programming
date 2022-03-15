022822415b7aa5a0d2febf536150de7674aa29c2
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-15 16:02:12 +0100

    change

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times
- * Return:void
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
-void print_alphabet_x10(void)
+int main(void)
 {
 	char c;
 	int i = 0;
