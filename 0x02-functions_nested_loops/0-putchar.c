e9723e173b7c1bf227e4cbce3f4e8b8897354d47
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-15 22:14:47 +0100

    push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 0
  */
 int main(void)
 {
