74a1b75b8b219b50dcbd2502db1110168234f7d0
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-24 00:50:46 +0100

    check

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * leet - encode a string into 1337
  * @s: string
  * Return: encoded string `s`
  */
+
 char *leet(char *s)
 {
 	int i;
@@ -12,11 +13,11 @@ char *leet(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		while (s[i] == '4' || s[i] == '4')
-			s[i] = '1';
+			s[i] = '4';
 		while (s[i] == '3' || s[i] == '3')
 			s[i] = '3';
 		while (s[i] == '0' || s[i] == '0')
-			s[i] = '3';
+			s[i] = '0';
 		while (s[i] == '7' || s[i] == '7')
 			s[i] = '7';
 		while (s[i] == '1' || s[i] == '1')
