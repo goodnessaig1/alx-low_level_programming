39bdafabe9796a33b5ae3d6e620ab129aa3a6d2c
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-24 00:41:48 +0100

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,11 +12,11 @@ char *leet(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
+			s[i] = '1';
 		while (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
 		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
+			s[i] = '3';
 		while (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
 		while (s[i] == 'l' || s[i] == 'L')
