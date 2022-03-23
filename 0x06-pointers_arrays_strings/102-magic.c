0e7343a75241033176efb7f9f3ffec1d38fc9889
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-23 23:54:41 +0100

    check

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 int main(void)
-{
-  int n;
-  int a[5];
-  int *p;
-  a[2] = 98;
-  p = &n;
+
   /*
    * write your line of code here...
    * Remember:
@@ -16,6 +11,12 @@ int main(void)
    */
   ;
   /* ...so that this prints 98\n */
+{
+  int n;
+  int a[5];
+  int *p;
+  a[2] = 1024;
+  p = &n;
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
